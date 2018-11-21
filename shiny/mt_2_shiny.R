dc8bf20a45657f6ed40bafbb55bd069a8bff74e9
Rana <rbansal@college.harvard.edu>
2018-11-20 22:24:25 -0500

    We update the titling of the axis.

--- shiny/mt_2_shiny.R
+++ shiny/mt_2_shiny.R
@@ -6,6 +6,13 @@ library(ggplot2)
 library(tidyverse)
 library(dplyr)
 
+myArray <-  c("vote_change" = "Raw Votes vs. 2016",
+              "kav_percentage" = "Kavanaugh Approval",
+              "feminism_percentage" = "Feminism Support",
+              "clint_margin" = "Clinton Margin",
+              "other_percentage" = "Other vote Percentage",
+              "dem_percentage" = "Democrat vote Percentage",
+              "cell_percentage" = "Cell Usage")
 
 project_data <- read_rds("./shiny_data.rds")
 
@@ -26,7 +33,12 @@ ui <- fluidPage(
                     "Clinton Margin" = "clint_margin",
                     "Other vote Percentage" = "other_percentage",
                     "Democrat vote Percentage" = "dem_percentage",
-                    "Cell Usage" = "cell_percentage"))
+                    "Cell Usage" = "cell_percentage")),
+      
+      "Above is a selection of interesting comparisons that we noticed when analyzing the dataset.
+      One main take away is the Raw Votes vs. 2016 tab. It is interesting that this variable almost
+      directly correlates with prediction accuracy. There is a chance that we could do a much better job
+      predicting the changes in raw votes, and that this could increase our accuracy in the future."
     ),
     
     # Show a plot of the generated distribution
@@ -45,13 +57,15 @@ server <- function(input, output) {
   
   output$distPlot <- renderPlot({
     
-    
+#     
     datareact() %>% 
       filter(UQ(as.name(input$data)) != 0) %>% 
       ggplot(aes_string(x = input$data, y = "poll_accuracy", color = "Party")) + 
       geom_smooth(method = lm) +
       geom_point(size = 6, alpha = .3) +
-      labs(caption = "Prediction Data from Upshot/Siena polling data")
+      labs(caption = "Prediction Data from Upshot/Siena polling data", 
+           x = myArray[input$data],
+           y = "Accuracy of Prediction")
   })
 }
 
